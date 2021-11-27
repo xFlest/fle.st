caea8b0421cf4ffebd2b6561eaad5fba4390a686
ふれすと <94770654+xFlest@users.noreply.github.com>
2021-11-27 18:23:47 +0900

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,15 @@
+$('.link-tw').on('click', function() {
+  window.location.href = 'https://twitter.com/xflest';
+});
+$('.link-in').on('click', function() {
+  window.location.href = 'https://www.instagram.com/zflest';
+});
+$('.link-li').on('click', function() {
+  window.location.href = 'https://line.me/ti/p/UvLrZbk76V';
+});
+$('.link-dc').on('click', function() {
+  window.location.href = 'https://discord.com/users/559273656342151179';
+});
+$('.link-yt').on('click', function() {
+  window.location.href = 'https://youtube.com/channel/UCjUBlrQbLSwC-7fRv47o-2w';
+});

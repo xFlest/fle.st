a3ba0d32926594328703026dd72f85f081d85feb
ふれすと <94770654+xFlest@users.noreply.github.com>
2022-02-05 20:03:06 +0900

    Delete redirect.js

--- script/redirect.js
+++
@@ -1,9 +0,0 @@
-const redrect = () => {
-  const url = location.href;
-  if (url = 'https://fle.st/tweet') {
-    location.href = 'https://fle.st';
-  } else {
-    location.href = 'https://google.com';
-  }
-};
-redrect();

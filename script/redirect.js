0491ae4354b74da21479518bdf4fe1b7a6951a66
ふれすと <94770654+xFlest@users.noreply.github.com>
2022-02-05 19:19:46 +0900

    Create redirect.js

---
+++ script/redirect.js
@@ -0,0 +1,9 @@
+const redrect = () => {
+  const pathnam = location.pathname;
+  if (pathnam = 'tweet') {
+    location.replace('https://fle.st');
+  } else {
+    console.log('pathname');
+  }
+};
+redrect();

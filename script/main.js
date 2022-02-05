cf6aae1c9e21fc08dcbb569a01a2d036151a0f37
ふれすと <94770654+xFlest@users.noreply.github.com>
2022-02-05 15:40:15 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,28 @@
-　
+const setFillHeight = () => {
+  const vh = $(window).height();
+  $(':root').css('--vh', `${vh}px`);
+}
+$(window).resize(setFillHeight);
+setFillHeight();
+const Interval = () => {
+  const cardPosition = $('.prf').offset();
+  const cardHeight = $('.prf').height();
+  const cardBottomY = cardPosition.top + cardHeight;
+  const contentPosition = $('.content-top-wave').offset();
+  const ccInterval = contentPosition.top - cardBottomY;
+  return ccInterval;
+};
+const firstInterval = Interval();
+const hideCard = () => {
+  const nowInterval = Interval();
+  const intervalRatio = nowInterval / firstInterval;
+  $('.prf').css('opacity',`${intervalRatio}`);
+  if (intervalRatio <= 0) {
+    $('.prf').css('pointer-events','none');
+  } else {
+    $('.prf').css('pointer-events','auto');
+  }
+};
+$('.content-scroller').scroll(function(){
+  hideCard();
+});

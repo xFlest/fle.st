2c92f89519a461c76187098586d08e31c02880ce
ふれすと <94770654+xFlest@users.noreply.github.com>
2022-02-11 21:53:01 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -18,13 +18,48 @@ const firstInterval = Interval();
 const hideCard = () => {
   const nowInterval = Interval();
   const intervalRatio = nowInterval / firstInterval;
-  $('.prf').css('opacity',`${intervalRatio}`);
+  $('.prf').css('opacity', `${intervalRatio}`);
   if (intervalRatio <= 0) {
-    $('.prf').css('pointer-events','none');
+    $('.prf').css('pointer-events', 'none');
   } else {
-    $('.prf').css('pointer-events','auto');
+    $('.prf').css('pointer-events', 'auto');
   }
 };
-$('.content-scroller').scroll(function(){
+$('.content-scroller').scroll(function() {
   hideCard();
 });
+
+const linkListScroll = () => $('.link-list').scrollLeft();
+const llScrollL = () => {
+  const itemWidth = $('.link-list li').width();
+  const gapWidth = $('.link-list').width() * 0.025;
+  const scroll = itemWidth + gapWidth;
+  return scroll;
+};
+$('.lln-left').on('click',function(){
+  const scrollTo = linkListScroll() - llScrollL();
+  $('.link-list').scrollLeft(scrollTo)
+});
+$('.lln-right').on('click', function() {
+  const scrollTo = linkListScroll() + llScrollL();
+  $('.link-list').scrollLeft(scrollTo)
+});
+const hideLlNav = () => {
+  if(linkListScroll() == 0){
+    $('.lln-left').addClass('hide-llnav')
+  } else if (linkListScroll() > 0) {
+    $('.lln-left').removeClass('hide-llnav')
+  }
+  const listWidth = $('.link-list').width();
+  const linkListScrollRight = linkListScroll() + listWidth;
+  const llScrollWidth = $('.link-list').get(0).scrollWidth;
+  if (linkListScrollRight >= llScrollWidth-1) {
+    $('.lln-right').addClass('hide-llnav')
+  } else if (linkListScrollRight < llScrollWidth-1) {
+    $('.lln-right').removeClass('hide-llnav')
+  }
+}
+$('.link-list').scroll(function(){
+  hideLlNav();
+});
+hideLlNav();
